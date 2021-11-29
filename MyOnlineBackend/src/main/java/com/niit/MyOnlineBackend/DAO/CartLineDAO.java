dd4c8dff2b530d36703b185cec05782d537c99c1
ghosh3428 <ghosh.debabrato@gmail.com>
2021-11-29 13:55:34 +0530

    Add files via upload

---
+++ MyOnlineBackend/src/main/java/com/niit/MyOnlineBackend/DAO/CartLineDAO.java
@@ -0,0 +1,25 @@
+package com.niit.MyOnlineBackend.DAO;
+
+import java.util.List;
+
+import com.niit.MyOnlineBackend.model.Cart;
+import com.niit.MyOnlineBackend.model.Cartline;
+
+public interface CartLineDAO 
+{
+	public List<Cartline> list(int cartId);
+	public Cartline get(int id);	
+	public boolean add(Cartline cartLine);
+	public boolean update(Cartline cartLine);
+	public boolean remove(Cartline cartLine);
+	
+	// fetch the CartLine based on cartId and productId
+	public Cartline getByCartAndProduct(int cartId, int productId);		
+		
+	// updating the cart
+	boolean updateCart(Cart cart);
+	
+	// list of available cartLine
+	public List<Cartline> listAvailable(int cartId);
+
+}
---
+++ MyOnlineBackend/src/main/java/com/niit/MyOnlineBackend/DAOIMPL/CartLineDAOIMPL.java
@@ -0,0 +1,104 @@
+package com.niit.MyOnlineBackend.DAOIMPL;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.niit.MyOnlineBackend.DAO.CartLineDAO;
+import com.niit.MyOnlineBackend.model.Cart;
+import com.niit.MyOnlineBackend.model.Cartline;
+
+public class CartLineDAOIMPL implements CartLineDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public Cartline getByCartAndProduct(int cartId, int productId) {
+		String query = "FROM Cartline WHERE cartId = :cartId AND product.id = :productId";
+		try {
+			
+			return sessionFactory.getCurrentSession()
+									.createQuery(query,Cartline.class)
+										.setParameter("cartId", cartId)
+										.setParameter("productId", productId)
+											.getSingleResult();
+			
+		}catch(Exception ex) {
+			return null;	
+		}
+		
+	}
+
+	@Override
+	public boolean add(Cartline cartLine) {
+		try {
+			sessionFactory.getCurrentSession().persist(cartLine);
+			return true;
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(Cartline cartLine) {
+		try {
+			sessionFactory.getCurrentSession().update(cartLine);
+			return true;
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean remove(Cartline cartLine) {	
+		try {			
+			sessionFactory.getCurrentSession().delete(cartLine);
+			return true;
+		}catch(Exception ex) {
+			return false;
+		}		
+	}
+
+
+	@Override
+	public List<Cartline> list(int cartId) {
+		String query = "FROM Cartline WHERE cartId = :cartId";
+		return sessionFactory.getCurrentSession()
+								.createQuery(query, Cartline.class)
+									.setParameter("cartId", cartId)
+										.getResultList();		
+	}
+
+	@Override
+	public Cartline get(int id) {		
+		return sessionFactory.getCurrentSession().get(Cartline.class, Integer.valueOf(id));
+	}
+
+	@Override
+	public boolean updateCart(Cart cart) {
+		try {			
+			sessionFactory.getCurrentSession().update(cart);			
+			return true;
+		}
+		catch(Exception ex) {
+			return false;
+		}
+	}
+
+	@Override
+	public List<Cartline> listAvailable(int cartId) {
+		String query = "FROM Cartline WHERE cartId = :cartId AND available = :available";
+		return sessionFactory.getCurrentSession()
+								.createQuery(query, Cartline.class)
+									.setParameter("cartId", cartId)
+									.setParameter("available", true)
+										.getResultList();
+	}
+
+}
